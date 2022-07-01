94239f408dec15b73c07f153dcac80b3a07521c1
Carl <cboettig@gmail.com>
2022-07-01 05:08:28 +0000

    preprocess aquatics

---
+++ aquatics.R
@@ -0,0 +1,13 @@
+## simplify the data (could be done in python)
+library(tidyverse)
+aquatics <- read_csv("https://data.ecoforecast.org/targets/aquatics/aquatics-targets.csv.gz")
+aquatics <- aquatics |>
+  select(!contains(c("depth", "sd"))) |>
+  group_by(time, siteID) |>
+  summarise(across(!any_of(c("time", "siteID")), .fns = mean, na.rm=TRUE),
+            .groups="drop")
+
+
+## consider GP-fill since python is failing to do that?
+
+write_csv(aquatics, "aquatics-targets.csv.gz")
